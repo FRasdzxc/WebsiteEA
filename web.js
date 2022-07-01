64074ddf13691e3aa52f82ba078b53e39eb9afcb
FRasdzxc <yuktsau@gmail.com>
2022-07-02 00:00:20 +0800

    add

--- web.js
+++ web.js
@@ -1,9 +1,6 @@
 function cMusic(music){
     document.getElementById('musicPlayer').src = music;
 }
-function backToIndex(){
-    window.location = "index.html";
-}
-function cWeb1(){
-    window.location = "website1.html";
+function cWebPage(url){
+    window.location = url;
 }
